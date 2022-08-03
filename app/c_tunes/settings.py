34caa29412a9dac94a236e21e6ba6555f57c8bda
Chris Bowen <cbowen216@gmail.com>
2022-08-02 21:41:26 -0700

    fix Host

--- app/c_tunes/settings.py
+++ app/c_tunes/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-772cjh!8jbmi$h)lm5hhojx_q*$rm%%*&3q^-02=+yuali945p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'ec2-13-56-63-45.us-west-1.compute.amazonaws.com']
 
 
 # Application definition
